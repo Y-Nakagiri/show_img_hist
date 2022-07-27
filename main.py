dfe635421dc9166496c899ae8d0a9b0d130ef950
Yuki Nakagiri <y.nakagiri.495@stn.nitech.ac.jp>
2022-07-27 17:47:39 +0900

    fix code

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from skimage.io import imread
 # download the image
 img_url = 'https://www.nagoya-info.jp/upload/spots/large/9250743815e41121f4ab73.jpg'
 
-im = imread(img_url))
+im = imread(img_url)
 
-st.image(im, caption = 'image from wikimedia commons',
-         use_column_width = True)
+st.image(im, caption='image from wikimedia commons',
+         use_column_width=True)
 
 
 # show histgram of all colors
-hist_red, _=np.histogram(im[:, :, 0], bins = 64)
-hist_green, _=np.histogram(im[:, :, 1], bins = 64)
-hist_blue, _=np.histogram(im[:, :, 2], bins = 64)
-hist=np.stack((hist_red, hist_green, hist_blue), axis = 1)
+hist_red, _ = np.histogram(im[:, :, 0], bins=64)
+hist_green, _ = np.histogram(im[:, :, 1], bins=64)
+hist_blue, _ = np.histogram(im[:, :, 2], bins=64)
+hist = np.stack((hist_red, hist_green, hist_blue), axis=1)
 
-df_hist=pd.DataFrame(hist, columns = ['R', 'G', 'B'])
+df_hist = pd.DataFrame(hist, columns=['R', 'G', 'B'])
 st.bar_chart(df_hist)
 
 
 # choose one color
-color=st.radio(
+color = st.radio(
     "choose R, G, or B",
     ('R', 'G', 'B'))
 if color == 'R':
